4c6137baddcb20fe57ac6dfa06a8dd63c7a0768c
Prashant Deepake <deepakeprashant1@gmail.com>
2023-02-15 17:50:47 +0530

    View Person Belong same city or state

--- src/com/bridgelabz/addressbook/collection/Repository.java
+++ src/com/bridgelabz/addressbook/collection/Repository.java
@@ -9,7 +9,7 @@ public class Repository {
     public final int NUMBER = 3;
     Map<String, List<Contacts>> map = new HashMap<>();
     List<Contacts> contacts = new ArrayList<>();
-
+    AddressBook addressBook = new AddressBook();
     Scanner scan = new Scanner(System.in);
 
     public Map<String, List<Contacts>> getReturnAddressBook() {
@@ -37,31 +37,38 @@ public class Repository {
     }
 
     public void editContact() {
-        System.out.println("Enter Person First Name");
-        String userName = scan.next();
-        for (int i = 0; i < contacts.size(); i++) {
-            if (userName.equals(contacts.get(i).firstName)) {
-                editContactInformation(i);
-                System.out.println("Successfully Edit Person Details !");
-                break;
-            }
+        Contacts editContactObject = findContactInObject();
+        if (editContactObject != null) {
+            editContactInformation(editContactObject);
+            System.out.println("Successfully Edit Person Details !");
+
         }
         System.out.println("Name Does Not Exist");
     }
 
     public void deleteInformation() {
-        System.out.println("Enter Person First Name");
-        String name = scan.next();
-        for (int i = 0; i < contacts.size(); i++) {
-            if (name.equals(contacts.get(i).firstName)) {
-                System.out.println("DELETED ALL INFORMATION "
-                        + contacts.get(i).firstName + " "
-                        + contacts.get(i).lastName);
-                contacts.remove(i);
-                break;
-            }
+        Contacts removeContact = findContactInObject();
+        if (removeContact != null) {
+            contacts.remove(removeContact);
+            System.out.println("Contact Deleted");
         }
         System.out.println("Name Does Not Exist");
+    }
+
+    private Contacts findContactInObject() {
+        String bookName = addressBook.choiceAddressBook(map);
+        Contacts removeObject = null;
+        if (bookName != null) {
+            System.out.println("Enter Person First Name");
+            String name = scan.next();
+            removeObject = map.get(bookName).stream()
+                    .filter(n -> n.firstName.equals(name))
+                    .findFirst()
+                    .orElse(null);
+            return removeObject;
+        }
+        System.out.println(bookName + "Book Not Available");
+        return removeObject;
     }
 
     public void searchPersonBelongCityOrState(String location) {
@@ -78,25 +85,25 @@ public class Repository {
                                 + "\n")));
     }
 
-    private void editContactInformation(int index) {
+    private void editContactInformation(Contacts editContactObject) {
         System.out.println("\tWHICH DATA EDIT \n\t1] FULL NAME \n\t2] ADDRESS " +
                 "\n\t3] PHONE NUMBER");
         int choice = scan.nextInt();
         switch (choice) {
             case NAME:
                 System.out.println("ENTER NEW FIRST NAME AND LAST NAME");
-                contacts.get(index).firstName = scan.next();
-                contacts.get(index).lastName = scan.next();
+                editContactObject.setFirstName(scan.next());
+                editContactObject.setLastName(scan.next());
                 break;
             case FULL_ADDRESS:
                 System.out.println("ENTER NEW ADDRESS , CITY AND STATE");
-                contacts.get(index).address = scan.next();
-                contacts.get(index).city = scan.next();
-                contacts.get(index).state = scan.next();
+                editContactObject.setAddress(scan.next());
+                editContactObject.setCity(scan.next());
+                editContactObject.setState(scan.next());
                 break;
             case NUMBER:
                 System.out.println(" ENTER NEW PHONE NUMBER");
-                contacts.get(index).phoneNumber = scan.next();
+                editContactObject.setPhoneNumber(scan.next());
                 break;
             default:
                 System.out.println("WRONG CHOICE ..THANK YOU");
